1d47c4ce0aed0d8d0dc5b453780f1cab4e40fd32
Vectormike <victorjonah199@gmail.com>
2021-04-28 00:44:20 +0100

    ...

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,5 @@ const transactionRoute = require('./transaction.route');
 const router = express.Router();
 
 router.use('/auth', authRoute);
-router.use('/transaction', transactionRoute);
 
 module.exports = router;
