b7d5fb906980b0d1ef558a09e060f06f053aa549
Vectormike <victorjonah199@gmail.com>
2021-05-16 15:12:17 +0100

    ...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,27 +1,12 @@
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
-const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const { userService } = require('../services');
+const { userService, referralService } = require('../services');
 
 const createUser = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
+  await referralService.createReferral(user.id);
+  await referralService.confirmReferral(req.body);
   res.status(httpStatus.CREATED).send(user);
-});
-
-const getUsers = catchAsync(async (req, res) => {
-  const filter = pick(req.query, ['name', 'role']);
-  const options = pick(req.query, ['sortBy', 'limit', 'page']);
-  const result = await userService.queryUsers(filter, options);
-  res.send(result);
-});
-
-const getUser = catchAsync(async (req, res) => {
-  const user = await userService.getUserById(req.params.userId);
-  if (!user) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
-  }
-  res.send(user);
 });
 
 const updateUser = catchAsync(async (req, res) => {
@@ -36,8 +21,6 @@ const deleteUser = catchAsync(async (req, res) => {
 
 module.exports = {
   createUser,
-  getUsers,
-  getUser,
   updateUser,
   deleteUser,
 };
